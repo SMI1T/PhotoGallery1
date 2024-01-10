2f1e0e3bc94b346d6518defde208585e1e233a4d
SMI1T <cahek-788@yandex.ru>
2024-01-10 19:18:34 +0700

    Realizoval PhotoHolder, v kotorom vivodyatsya kartinki i po najatiu na cotorie oni sohrabyautsya na usrtoystve

---
+++ app/src/main/java/com/sample/photogallery/PhotoGalleryFragment.kt
@@ -0,0 +1,214 @@
+package com.sample.photogallery
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.work.Constraints
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.NetworkType
+import androidx.work.PeriodicWorkRequest
+import androidx.work.WorkManager
+import com.squareup.picasso.Picasso
+import java.util.concurrent.TimeUnit
+
+private const val TAG = "PhotoGalleryFragment"
+private const val POLL_WORK = "POLL_WORK"
+class PhotoGalleryFragment : Fragment() {
+    /**
+     * Требуемый интерфейс
+     */
+    interface Callbacks {
+        fun onDatabaseSelected()
+        fun onAddSelected(galleryItem: GalleryItem)
+        fun onDeleteSelected()
+    }
+
+    private var callbacks: Callbacks? = null
+    private lateinit var photoGalleryViewModel: PhotoGalleryViewModel
+    private lateinit var photoRecyclerView: RecyclerView
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        callbacks = context as Callbacks?
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+        photoGalleryViewModel =
+            ViewModelProviders.of(this).get(PhotoGalleryViewModel::class.java)
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val view = inflater.inflate(R.layout.fragment_photo_gallery, container, false)
+        photoRecyclerView = view.findViewById(R.id.photo_recycler_view)
+        photoRecyclerView.layoutManager = GridLayoutManager(context, 3)
+        return view
+    }
+    override fun onViewCreated(view: View,
+                               savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        photoGalleryViewModel.galleryItemLiveData.observe(
+            viewLifecycleOwner,
+            Observer { galleryItems ->
+                photoRecyclerView.adapter =
+                    PhotoAdapter(galleryItems)
+            })
+    }
+    override fun onDetach() {
+        super.onDetach()
+        callbacks = null
+    }
+
+    private inner class PhotoHolder(itemImageView: ImageView) : RecyclerView.ViewHolder(itemImageView), View.OnClickListener {
+        val bindImageView: (ImageView) = itemImageView
+        //val titleTextView: TextView = itemView.findViewById(R.id.photo_title)
+        private lateinit var galleryItem: GalleryItem
+        init {
+            bindImageView.setOnClickListener(this)
+        }
+        fun bind(galleryItem: GalleryItem) {
+
+            this.galleryItem = galleryItem
+            Picasso.get()
+                .load(galleryItem.url)
+                .placeholder(R.drawable.bill_up_close)
+                .into(bindImageView)
+            //titleTextView.text = galleryItem.title
+        }
+        override fun onClick(v: View) {
+            Toast.makeText(
+                context,
+                "Добавлено фото:${galleryItem.title}",
+                Toast.LENGTH_SHORT
+            ).show()
+            callbacks?.onAddSelected(galleryItem)
+        }
+    }
+
+    private inner class PhotoAdapter(private val galleryItems: List<GalleryItem>)
+        : RecyclerView.Adapter<PhotoHolder>()  {
+
+        override fun onCreateViewHolder(
+            parent: ViewGroup,
+            viewType: Int
+        ): PhotoHolder {
+            val view = layoutInflater.inflate(
+                R.layout.list_item_gallery,
+                parent,
+                false
+            ) as ImageView
+            return PhotoHolder(view)
+        }
+        override fun getItemCount(): Int = galleryItems.size
+        override fun onBindViewHolder(holder: PhotoHolder, position: Int) {
+            val galleryItem = galleryItems[position]
+            Picasso.get()
+                .load(galleryItem.url)
+                .placeholder(R.drawable.bill_up_close)
+                .into(holder.bindImageView)
+            holder.bind(galleryItem)
+        }
+    }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.fragment_photo_gallery, menu)
+        val searchItem: MenuItem =
+            menu.findItem(R.id.menu_item_search)
+        val searchView = searchItem.actionView
+                as SearchView
+        searchView.apply {
+            setOnQueryTextListener(object :
+                SearchView.OnQueryTextListener {
+                override fun
+                        onQueryTextSubmit(queryText: String): Boolean {
+                    Log.d(TAG,
+                        "QueryTextSubmit: $queryText")
+                    photoGalleryViewModel.fetchPhotos(queryText)
+                    return true
+                }
+                override fun
+                        onQueryTextChange(queryText: String): Boolean {
+                    Log.d(TAG,
+                        "QueryTextChange: $queryText")
+                    return false
+                }
+            })
+            setOnSearchClickListener {
+                searchView.setQuery(photoGalleryViewModel.searchTerm, false)
+            }
+        }
+        val toggleItem =
+            menu.findItem(R.id.menu_item_toggle_polling)
+        val isPolling =
+            QueryPreferences.isPolling(requireContext())
+        val toggleItemTitle = if (isPolling) {
+            R.string.stop_polling
+        } else {
+            R.string.start_polling
+        }
+        toggleItem.setTitle(toggleItemTitle)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_item_clear -> {
+                photoGalleryViewModel.fetchPhotos("")
+                true
+            }
+            R.id.menu_item_toggle_polling -> {
+                val isPolling = QueryPreferences.isPolling(requireContext())
+                if (isPolling) {
+                    WorkManager.getInstance().cancelUniqueWork(POLL_WORK)
+                    QueryPreferences.setPolling(requireContext(), false)
+                } else {
+                    val constraints = Constraints.Builder().setRequiredNetworkType(NetworkType.UNMETERED).build()
+                    val periodicRequest = PeriodicWorkRequest
+                            .Builder(PollWorker::class.java, 15, TimeUnit.MINUTES)
+                            .setConstraints(constraints)
+                            .build()
+                    WorkManager.getInstance().enqueueUniquePeriodicWork(POLL_WORK,
+                        ExistingPeriodicWorkPolicy.KEEP, periodicRequest)
+                    QueryPreferences.setPolling(requireContext(), true)
+                }
+                activity?.invalidateOptionsMenu()
+                return true
+            }
+            R.id.menu_item_database_photos -> {
+                callbacks?.onDatabaseSelected()
+                true
+            }
+            R.id.menu_item_delete_database_photos -> {
+                callbacks?.onDeleteSelected()
+                Toast.makeText(
+                    context,
+                    R.string.delete_photos_from_database_success,
+                    Toast.LENGTH_SHORT
+                ).show()
+                true
+            }
+            else ->
+                super.onOptionsItemSelected(item)
+        }
+    }
+
+
+    companion object {
+        fun newInstance() = PhotoGalleryFragment()
+    }
+}
